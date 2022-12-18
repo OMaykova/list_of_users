00b15bccc16e46571701a74004665018a0fe99c8
Olga Maykova <dolkaaaaa@gmail.com>
2022-12-18 08:46:04 +0300

    feat: layout profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from '../Header/Header.jsx';
 import Main from '../Main/Main';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
     <div className="page">
-      <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<><Header /><Main /></>} />
+        <Route path="/profile" element={<><Header /><Profile /></>} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import './Header.css';
+import avaTest from '../../images/ava_test.svg';
 
 function Header() {
-  return (
-    <section className="header">
-      <Link to = '/signout' className='header__signout-link'>
-        <button className='header__signout-btn' title='Выход' type="button" aria-label="Кнопка выход">Выход</button>
-      </Link>
-      <div className="team">
-        <h1 className="team__title">Наша команда</h1>
-        <p className="team__description">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций.</p>
-      </div>
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    </section>
+  return (
+    <>
+      {location.pathname === '/' &&
+        <section className="header header_type_main">
+          <Link to = '/signout' className='header__btn-cover header__btn-cover_type_exit'>
+            <button className='header__btn header__btn_type_exit' title='Выход' type="button" aria-label="Кнопка выход">Выход</button>
+          </Link>
+          <div className="team">
+            <h1 className="team__title">Наша команда</h1>
+            <p className="team__description">Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций.</p>
+          </div>
+        </section>
+      }
+      {location.pathname === '/profile' &&
+        <section className="header header_type_profile">
+          <div className="header__container"> 
+            <div className="header__btn-cover header__btn-cover_type_back">   
+              <button className='header__btn header__btn_type_back' onClick={() => navigate(-1)} title='Назад' type="button" aria-label="Кнопка назад">Назад</button>
+            </div>
+            <img className="header__avatar" src={avaTest} alt='Фото профиля' />
+            <div className="header__description">
+              <h1 className="header__name">Фамилия</h1>
+              <p className="header__role">Партнер</p>
+            </div>
+          </div>
+          <Link to = '/signout' className='header__btn-cover header__btn-cover_type_exit'>
+            <button className='header__btn header__btn_type_exit' title='Выход' type="button" aria-label="Кнопка выход">Выход</button>
+          </Link>
+        </section>
+      }
+    </>
   );
 }
 
